ef043c0cc99dad18127b3a4c0924a98a38f5f878
lszero <lszero@users.noreply.github.com>
2017-04-08 23:22:59 +0800

    Update solution_2.cpp

--- leetcode/12-Integer to Roman/solution_2.cpp
+++ leetcode/12-Integer to Roman/solution_2.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-	string C[13] = {"M", "CM", "D", "CD",  "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-	int    D[13] = {1000, 900, 500, 400,   100,  90,   50,  40,   10,  9,    5,   4,    1 };
+    string C[13] = {"M", "CM", "D", "CD",  "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int    D[13] = {1000, 900, 500, 400,   100,  90,   50,  40,   10,  9,    5,   4,    1 };
  
     string intToRoman(int num) {
-    	string ans = "";
+        string ans = "";
  
         for(int i = 0; i < 13; i++){
-        	while(num >= D[i]){
-        		num -= D[i];
-        		ans += C[i];
-        	}
+            while(num >= D[i]){
+                num -= D[i];
+                ans += C[i];
+            }
         }
         return ans;
     } 
